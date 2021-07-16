f05a81202b3aaa42c003ec1002c9531d6740b3a3
Raul Torres <jr.torsa@gmail.com>
2021-07-15 23:40:39 -0600

    empezando a trabajar en los posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data";
+import "./app.css";
 
 function App() {
+  const [votedPosts, setVotedPosts] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Blog Posts Populares</h1>
+
+      <ul>
+        {data.map((post) => (
+          <>
+            <img src={post.post_image_url} />
+
+            <p>{post.title}</p>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }

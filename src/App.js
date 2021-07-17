2120e08be6683a8f89b4386281923754928033f0
Raul Torres <jr.torsa@gmail.com>
2021-07-17 00:29:53 -0600

    agregando funciones para incrementar y decrementar votos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import data from "./data";
 import { Container, Header, Grid, Image, Icon, Button } from "semantic-ui-react";
 
 function App() {
-  const [votedPosts, setVotedPosts] = useState(0);
+
+  const [counter, setCounter] = useState(1)
+
+  const onIncrement = id => {
+    const vote = data.map( post => {
+      if(id === post.id){
+        post.votes = post.votes + 1
+      }
+      return post.votes
+    })
+    setCounter(vote)
+  }
+  
+  const onDecrement = id => {
+    const vote = data.map( post => {
+      if(id === post.id){
+        post.votes = post.votes - 1
+      }
+      return post.votes
+    })
+    setCounter(vote)
+  }
 
   return (
     <Container>
@@ -18,24 +39,24 @@ function App() {
         </Grid.Row>
       </Grid>
       {data.map((post) => (
-        <Grid columns={3}>
+        <Grid key={post.id} columns={3}>
           <Grid.Column>
             <Image src={post.post_image_url} size="medium" />
           </Grid.Column>
           <Grid.Column verticalAlign="middle" width={2}>
             <Grid.Row>
-              <Icon name="angle up" />
+              <Icon onClick={() => onIncrement(post.id)} name="angle up" />
             </Grid.Row>
             <h4 style={{paddingBottom: '10px'}}>{post.votes}</h4>
             <Grid.Row>
-              <Icon name="angle down" />
+              <Icon onClick={() => onDecrement(post.id)} name="angle down" />
             </Grid.Row>
           </Grid.Column>
           <Grid.Column>
             <Container text textAlign="left">
-              <Header as="h4" color="blue">
+              <a href={post.url}>
                 {post.title}
-              </Header>
+              </a>
               <p>{post.description}</p>
               <span style={{ color: "blue" }}>Escrito por:</span>
               <Image src={post.writer_avatar_url} size="mini" circular spaced />

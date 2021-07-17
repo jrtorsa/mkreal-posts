fc1c6d8a2dd9d739c464d52df476b5922318efe7
Raul Torres <jr.torsa@gmail.com>
2021-07-16 22:56:03 -0600

    agregando semantic ui y dejando el sitio como en el mock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import { useState } from "react";
 import data from "./data";
-import "./app.css";
+import { Container, Header, Grid, Image, Icon, Button } from "semantic-ui-react";
 
 function App() {
   const [votedPosts, setVotedPosts] = useState(0);
 
   return (
-    <div className="container">
-      <h1>Blog Posts Populares</h1>
-
-      <ul>
-        {data.map((post) => (
-          <>
-            <img src={post.post_image_url} />
-
-            <p>{post.title}</p>
-          </>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <Header as="h1" textAlign="center" dividing>
+        Blog Posts Populares
+      </Header>
+      <Grid>
+        <Grid.Row>
+          <p style={{padding: '10px 10px 0px 0px', fontWeight: 'bold'}}>Orden: </p>
+        <Button primary basic>Ascendete</Button>
+        <Button primary>Descendente</Button>
+        </Grid.Row>
+      </Grid>
+      {data.map((post) => (
+        <Grid columns={3}>
+          <Grid.Column>
+            <Image src={post.post_image_url} size="medium" />
+          </Grid.Column>
+          <Grid.Column verticalAlign="middle" width={2}>
+            <Grid.Row>
+              <Icon name="angle up" />
+            </Grid.Row>
+            <h4 style={{paddingBottom: '10px'}}>{post.votes}</h4>
+            <Grid.Row>
+              <Icon name="angle down" />
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column>
+            <Container text textAlign="left">
+              <Header as="h4" color="blue">
+                {post.title}
+              </Header>
+              <p>{post.description}</p>
+              <span style={{ color: "blue" }}>Escrito por:</span>
+              <Image src={post.writer_avatar_url} size="mini" circular spaced />
+            </Container>
+          </Grid.Column>
+        </Grid>
+      ))}
+    </Container>
   );
 }
 
